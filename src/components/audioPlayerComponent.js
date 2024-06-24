db20cf23ff740ce0ae685ebde4736b07260a2982
Achintha Bandara <achinthabandara1@gmail.com>
2024-06-24 19:14:58 +0530

    play audio on focus

---
+++ src/components/audioPlayerComponent.js
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react';
+import {View, Button, BackHandler} from 'react-native';
+import BackgroundTimer from 'react-native-background-timer';
+
+import log from '../config/logger';
+import audioFocusService from '../services/audioFocusService';
+import audioManagerService from '../services/soundManagerService';
+
+const AudioPlayerComponent = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [sound, setSound] = useState(null);
+
+  const playAudio = async () => {
+    try {
+      const soundPath = 'https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/start.ogg';
+      await audioFocusService.requestAudioFocus();
+      audioManagerService.playSound(soundPath, setSound);
+    } catch (error) {
+      log.error('Error requesting audio focus:', error);
+    }
+  };
+
+  const stopPlaying = () => {
+    BackgroundTimer.stopBackgroundTimer();
+    setIsPlaying(false);
+    audioManagerService.stopSound(sound, setSound);
+  };
+
+  const startPlaying = () => {
+    setIsPlaying(true);
+    BackHandler.exitApp();
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      BackgroundTimer.runBackgroundTimer(() => {
+        playAudio();
+      }, 10000);
+    } else {
+      BackgroundTimer.stopBackgroundTimer();
+    }
+    return () => {
+      BackgroundTimer.stopBackgroundTimer();
+    };
+  }, [isPlaying]);
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button title="Start" onPress={startPlaying} />
+      <Button title="Stop" onPress={stopPlaying} />
+    </View>
+  );
+};
+
+export default AudioPlayerComponent;
---
+++ src/services/soundManagerService.js
@@ -0,0 +1,40 @@
+import Sound from 'react-native-sound';
+import audioFocusService from './audioFocusService';
+
+import log from '../config/logger';
+
+const playSound = (audio, setSound) => {
+  Sound.setCategory('Playback', true);
+
+  const newSound = new Sound(audio, Sound.MAIN_BUNDLE, error => {
+    if (error) {
+      log.error('Failed to load the sound', error);
+      return;
+    }
+    newSound.play(success => {
+      if (success) {
+        audioFocusService.abandonAudioFocus();
+      } else {
+        log.error('Failed to play the sound');
+      }
+    });
+  });
+  setSound(newSound);
+};
+
+const stopSound = (sound, setSound) => {
+  audioFocusService.abandonAudioFocus();
+  if (sound) {
+    sound.stop(() => {
+      sound.release();
+      setSound(null);
+    });
+  }
+};
+
+const audioManagerService = {
+  playSound,
+  stopSound,
+};
+
+export default audioManagerService;
